e981053e90647f92f5458219e577bab8ba48af34
Abhinandan <nmabhinandan@gmail.com>
2015-08-06 16:32:22 +0530

    I'm embarrassed

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-exports['default'] = function (req, res, next) {
-  if (req.header('X-PJAX')) {
-    res.render = function (view, options, callback) {
-      if (options === undefined) options = {};
+exports['default'] = function () {
+  return function (req, res, next) {
+    if (req.header('X-PJAX')) {
+      res.render = function (view, options, callback) {
+        if (options === undefined) options = {};
 
-      var done = callback;
-      var opts = options;
+        var done = callback;
+        var opts = options;
 
-      if (typeof options === 'function') {
-        done = options;
-        opts = {};
-      }
-      if (!done) {
-        done = function (err, str) {
-          if (err) return req.next(err);
-          res.send(str);
-        };
-      }
-      opts.layout = false;
-      req.app.render(view, opts, done);
-    };
-  }
-  next();
+        if (typeof options === 'function') {
+          done = options;
+          opts = {};
+        }
+        if (!done) {
+          done = function (err, str) {
+            if (err) return req.next(err);
+            res.send(str);
+          };
+        }
+        opts.layout = false;
+        req.app.render(view, opts, done);
+      };
+    }
+    next();
+  };
 };
 
 module.exports = exports['default'];
